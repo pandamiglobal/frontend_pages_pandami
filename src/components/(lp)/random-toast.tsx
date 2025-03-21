2ec2d0cc8891ab64883ccae49b27e94e9cd5b189
Rodrigo da silva <rodrigo.bs.548@gmail.com>
2025-03-21 12:48:29 -0500

    fix: correções de build

--- src/components/(lp)/random-toast.tsx
+++ src/components/(lp)/random-toast.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect, useRef } from "react"
-import { cn } from "@/lib/utils"
+import { cn } from "@/common/lib/utils"
 
 const blinkAnimation = `
   @keyframes blink {
