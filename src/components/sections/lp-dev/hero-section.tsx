4dcf75c512b1721ea16d656dccb2ec6c3e733e92
Rodrigo da silva <rodrigo.bs.548@gmail.com>
2025-05-01 14:12:38 -0500

    feat: adiciona o globo na hero

--- src/components/sections/lp-dev/hero-section.tsx
+++ src/components/sections/lp-dev/hero-section.tsx
@@ -1,9 +1,10 @@
 import { Container } from "@/components/ui/container"
 import Link from "next/link"
+import { GlobeAlternative } from "./globe-alternative"
 
 export default function HeroSection() {
   return (
-    <section className="snap-start relative min-h-screen flex items-center justify-center overflow-hidden">
+    <section className="snap-start min-h-screen flex pt-12 justify-center overflow-hidden relative">
       {/* Background com grid pattern e gradiente animado */}
       <div className="absolute inset-0 bg-[#F3F4F6]">
         <div className="absolute inset-0 bg-grid-pattern"></div>
@@ -22,19 +23,21 @@ export default function HeroSection() {
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Link
               href="#registrar"
-              className="py-4 px-2 md:px-6 rounded-full flex items-center justify-start relative overflow-hidden disabled:opacity-50 disabled:cursor-not-allowed bg-[#f47834] text-white font-medium hover:bg-[#e06724] transition-colors"
+              className="py-4 px-2 md:px-6 rounded-full flex items-center justify-center md:justify-start relative overflow-hidden disabled:opacity-50 disabled:cursor-not-allowed bg-[#f47834] text-white font-medium hover:bg-[#e06724] transition-colors"
             >
               Registrar Minha Marca
             </Link>
             <Link
               href="#consultoria"
-              className="py-4 px-2 md:px-6 rounded-full flex items-center justify-start relative overflow-hidden disabled:opacity-50 disabled:cursor-not-allowed bg-white text-[#f47834] border-2 border-[#f47834] font-medium hover:bg-gray-50 transition-colors"
+              className="py-4 px-2 md:px-6 rounded-full flex items-center justify-center md:justify-start relative overflow-hidden disabled:opacity-50 disabled:cursor-not-allowed bg-white text-[#f47834] border-2 border-[#f47834] font-medium hover:bg-gray-50 transition-colors"
             >
               Consultoria em Cibersegurança
             </Link>
           </div>
         </div>
       </Container>
+      
+      <GlobeAlternative className="absolute bottom-[-150px] min-w-[400px]:bottom-[-200px] sm:bottom-[-300px] left-1/2 -translate-x-1/2" />
     </section>
   )
 }
