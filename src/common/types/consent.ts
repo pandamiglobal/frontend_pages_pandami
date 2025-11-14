650d8cb4d88fea7a2fc13b86d33b2c2ea1fc0791
kelmeofc <kelme.dev@pm.me>
2025-11-14 16:29:13 -0300

    fix: folders organization

--- src/common/types/consent.ts
+++
@@ -1,6 +0,0 @@
-export type ConsentChoice = "accepted" | "essentials_only" | "denied";
-
-export interface WindowWithGtag {
-  gtag?: (...args: any[]) => void;
-  clarity?: (command: string, value: boolean) => void;
-}
