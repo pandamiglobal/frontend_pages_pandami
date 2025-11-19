4f791939396fe55d9d20404cdc325550d0c10fea
kelmeofc <kelme.dev@pm.me>
2025-11-19 12:27:37 -0300

    fix: remove error and not-found for pubic profile

--- src/app/(public)/p/[public-profile]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { PublicProfileErrorState } from '@/app/_components/pages/public-profile-page/public-profile-error-state'
-
-/**
- * Custom not-found page for public profiles
- * Friendly message when profile is not found
- */
-export default function PublicProfileNotFound() {
-  return <PublicProfileErrorState type="not_found" />
-}
